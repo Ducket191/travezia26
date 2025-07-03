c451b413d4039b89117ea427c11a1c955f510dae
Ducket191 <dangminhduc1912008@gmail.com>
2025-07-03 15:11:16 +0700

    test

--- sever/index.js
+++ sever/index.js
@@ -63,7 +63,7 @@ app.post('/create-payment-link', async (req, res) => {
 
       const order = {
         amount,
-        description: "thanh toan",
+        description: "1111111111111111111111111",
         orderCode,
         returnUrl: `${YOUR_DOMAIN}/success.html`,
         cancelUrl: `${YOUR_DOMAIN}/cancel.html`
