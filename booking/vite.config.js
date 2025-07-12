08ead0e075dc40f459e3e5722d73b9c8cdc06e72
Ducket191 <dangminhduc1912008@gmail.com>
2025-07-12 20:26:25 +0700

    update

--- booking/vite.config.js
+++ booking/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/travezia26/', 
+  base: './', // ✅ relative path for Netlify root deployment
 });
