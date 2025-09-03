5d995d009fed5baef3346d360ada1909eb79c0d9
Ducket191 <dangminhduc1912008@gmail.com>
2025-09-03 11:14:27 +0700

    update

---
+++ sever/model/infor.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const TicketInfoSchema = new mongoose.Schema({
+    Name: String,
+    Phone: String,
+    Email: String,
+    Ticket: int,
+});
+
+const TicketInforModel = mongoose.model('', TicketInfoSchema);
+
+module.exports = TicketInforModel;
